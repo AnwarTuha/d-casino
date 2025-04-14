47c7027647d353c06a372f9cf8033c3852d39664
AnwarTuha <anwartuha2@gmail.com>
2025-04-14 11:35:39 +0300

    update

--- main.dart.js
+++ main.dart.js
@@ -169130,12 +169130,14 @@
               t2 === $ && A.throwUnnamedLateFieldNI();
               t4 = type$.HttpResponse_PaginatedLotteryModel;
               _options = $async$self._setStreamType$1$1(t3.copyWith$1$baseUrl($async$self._lottery_detail_api_services$_combineBaseUrls$2(t2, $async$self.baseUrl)), t4);
+              t2 = _options.___RequestConfig__headers_A;
+              t2 === $ && A.throwUnnamedLateFieldNI();
+              A.print(">>>>>>>>>>>>>>>>>>>>>>>>> " + t2.toString$0(0));
               $async$goto = 3;
               return A._asyncAwait(t1.fetch$1$1(0, _options, type$.Map_String_dynamic), $async$getPaginatedLotteries$3$categoryId$page$pageSize);
             case 3:
               // returning from await.
               _result = $async$result;
-              J.toString$0$(_result.data);
               _value = A._Cell$named("_value");
               try {
                 t1 = _result.data;
