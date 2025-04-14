a11404ab57d04ae7c117334b8a7a17bf3388c2b3
AnwarTuha <anwartuha2@gmail.com>
2025-04-14 13:25:50 +0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2048279429"
+    serviceWorkerVersion: "3005003141"
   }
 });
