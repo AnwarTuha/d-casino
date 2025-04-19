7b793dc42775e207b857b73b842ed891427e5889
AnwarTuha <anwartuha2@gmail.com>
2025-04-19 17:44:03 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2a6ed6fcef65bd00e72ea334603ad555
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "3046c3932522416db988a828949b8bc2",
+"flutter_bootstrap.js": "e24194cba5e89c6de911d4d9781448e5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
